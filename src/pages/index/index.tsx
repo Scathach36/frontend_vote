fe4966e7b74bec4a211cb2ebce4c4f50e04641e8
Scathach <scathach0822@hotmail.com>
2023-03-14 19:49:41 +0800

    权限判定优化

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,10 +1,19 @@
 import { View, Text } from "@tarojs/components";
 import Taro from "@tarojs/taro";
-import { OsTabs, OsTabsHeader, OsTabsHeaderItem, OsTabsPanel } from "ossaui";
-import { useEffect } from "react";
+import {
+  OsButton,
+  OsNoticeBar,
+  OsTabs,
+  OsTabsHeader,
+  OsTabsHeaderItem,
+  OsTabsPanel,
+} from "ossaui";
+import { useEffect, useState } from "react";
 import "./index.scss";
 
 const index = () => {
+  const [current, setCurrent] = useState<number>(1);
+
   //   token校验
   useEffect(() => {
     const token = Taro.getStorageSync("token") || null;
@@ -22,23 +31,71 @@ const index = () => {
       });
     }
   });
+
+  // 退出系统
+  const quit = () => {
+    Taro.clearStorageSync();
+    Taro.redirectTo({ url: "/pages/login/index" });
+  };
+
   return (
     <>
-      {Taro.getStorageSync("role") == "0" && (
-        <>
-          <view>学生</view>
-        </>
-      )}
-      {Taro.getStorageSync("role") == "1" && (
-        <>
-          <view>教师</view>
-        </>
-      )}
-      {Taro.getStorageSync("role") == "2" && (
       <>
-        <view>管理员</view>
+        <OsNoticeBar leftIcon="inform" scrollable>
+          如果需要更改学号、班级、姓名等个人信息，请联系管理员帮助修改。
+        </OsNoticeBar>
+        <OsTabs
+          renderHeader={
+            <OsTabsHeader value={current}>
+              {Taro.getStorageSync("role") > 0 && (
+                <OsTabsHeaderItem
+                  index={0}
+                  current={current}
+                  text="发起投票"
+                  onClick={(value) => setCurrent(value)}
+                />
+              )}
+              <OsTabsHeaderItem
+                index={1}
+                current={current}
+                text="参与投票"
+                onClick={(value) => setCurrent(value)}
+              />
+              <OsTabsHeaderItem
+                index={2}
+                current={current}
+                text="我的"
+                onClick={(value) => setCurrent(value)}
+              />
+            </OsTabsHeader>
+          }
+        >
+          <OsTabsPanel
+            current={current}
+            index={0}
+            customStyle={{ height: Taro.pxTransform(400) }}
+          >
+            <View className="tabs__content">发起投票</View>
+          </OsTabsPanel>
+          <OsTabsPanel
+            current={current}
+            index={1}
+            customStyle={{ height: Taro.pxTransform(400) }}
+          >
+            <View className="tabs__content">参与投票</View>
+          </OsTabsPanel>
+          <OsTabsPanel
+            current={current}
+            index={2}
+            customStyle={{ height: Taro.pxTransform(400) }}
+          >
+            <OsButton type="primary" size="block" onClick={quit}>
+              退出系统
+            </OsButton>
+          </OsTabsPanel>
+        </OsTabs>
       </>
-      )}
+      )
     </>
   );
 };
