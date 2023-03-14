211bd1b538fc818b3463d6ccd6024b5996bb2dcd
Scathach <scathach0822@hotmail.com>
2023-03-14 14:57:51 +0800

    用户登录完成

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,24 +1,33 @@
-import { Component, PropsWithChildren } from 'react'
-import { View, Text } from '@tarojs/components'
-import './index.scss'
+import { View, Text } from "@tarojs/components";
+import Taro from "@tarojs/taro";
+import { useEffect } from "react";
+import "./index.scss";
 
-export default class Index extends Component<PropsWithChildren> {
-
-  componentWillMount () { }
-
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
-
-  render () {
-    return (
-      <View className='index'>
+const index = () => {
+  //   token校验
+  useEffect(() => {
+    const token = Taro.getStorageSync("token") || null;
+    if (token !== null) {
+      Taro.request({
+        url: "http://localhost:8080/login/checkToken",
+        method: "POST",
+        header: { token },
+      }).then((res) => {
+        if (!res) {
+          Taro.redirectTo({
+            url: "/pages/login/login",
+          });
+        }
+      });
+    }
+  });
+  return (
+    <>
+      <View className="index">
         <Text>Hello world!</Text>
       </View>
-    )
-  }
-}
+    </>
+  );
+};
+
+export default index;
