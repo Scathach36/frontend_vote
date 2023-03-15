855d078dc59a42de55f1f99114d3ab1ecdfe09a4
Scathach <scathach0822@hotmail.com>
2023-03-15 19:59:14 +0800

    底部tab栏根据不同权限显示不同菜单

--- src/custom-tab-bar/index.tsx
+++ src/custom-tab-bar/index.tsx
@@ -1,28 +1,76 @@
 import { OsTabBar } from "ossaui";
 import "../../node_modules/ossaui/dist/style/components/tab-bar.scss";
 import "ossaui/dist/style/components/icon.scss";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { View } from "@tarojs/components";
 import Taro from "@tarojs/taro";
-
-const tabsArr = [
-  {
-    icon: "home",
-    selectedIcon: "home-pressed",
-    text: "首页",
-    path: "/pages/index/index",
-  },
-  {
-    icon: "user",
-    selectedIcon: "user-pressed",
-    text: "我的",
-    path: "/pages/user/index",
-  },
-];
 
 export default () => {
   // 需要使用全局状态管理current，因为每个页面的tabbar都是不同的实例
   const [current, setCurrent] = useState<number>(0);
+
+  const tabsArrForStudent: any[] = [
+    {
+      icon: "home",
+      selectedIcon: "home-pressed",
+      text: "首页",
+      path: "/pages/index/index",
+    },
+    {
+      icon: "user",
+      selectedIcon: "user-pressed",
+      text: "我的",
+      path: "/pages/user/index",
+    },
+  ];
+
+  const tabsArrForTeacher: any[] = [
+    {
+      icon: "home",
+      selectedIcon: "home-pressed",
+      text: "首页",
+      path: "/pages/index/index",
+    },
+    {
+      icon: "subject",
+      selectedIcon: "subject-pressed",
+      text: "投票统计",
+      path: "/pages/index/index",
+    },
+    {
+      icon: "user",
+      selectedIcon: "user-pressed",
+      text: "我的",
+      path: "/pages/user/index",
+    },
+  ];
+
+  const tabsArrForAdmin: any[] = [
+    {
+      icon: "home",
+      selectedIcon: "home-pressed",
+      text: "首页",
+      path: "/pages/index/index",
+    },
+    {
+      icon: "subject",
+      selectedIcon: "subject-pressed",
+      text: "投票管理",
+      path: "/pages/index/index",
+    },
+    {
+      icon: "my-group-buying",
+      selectedIcon: "my-group-buying-select",
+      text: "用户管理",
+      path: "/pages/index/index",
+    },
+    {
+      icon: "user",
+      selectedIcon: "user-pressed",
+      text: "我的",
+      path: "/pages/user/index",
+    },
+  ];
 
   const onChange = (item, index) => {
     Taro.switchTab({
@@ -33,7 +81,27 @@ export default () => {
 
   return (
     <View>
-      <OsTabBar tabsArr={tabsArr} activeTabIdx={current} onClick={onChange} />
+      {Taro.getStorageSync("role") == 0 && (
+        <OsTabBar
+          tabsArr={tabsArrForStudent}
+          activeTabIdx={current}
+          onClick={onChange}
+        />
+      )}
+      {Taro.getStorageSync("role") == 1 && (
+        <OsTabBar
+          tabsArr={tabsArrForTeacher}
+          activeTabIdx={current}
+          onClick={onChange}
+        />
+      )}
+      {Taro.getStorageSync("role") == 2 && (
+        <OsTabBar
+          tabsArr={tabsArrForAdmin}
+          activeTabIdx={current}
+          onClick={onChange}
+        />
+      )}
     </View>
   );
 };
