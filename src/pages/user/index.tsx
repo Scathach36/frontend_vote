9c33fc2f631a0efc645d1a6bcfa36febac096735
Scathach <scathach0822@hotmail.com>
2023-03-15 20:46:55 +0800

    增加退出系统二次确认弹框

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,9 +1,13 @@
 import Taro from "@tarojs/taro";
-import { OsButton } from "ossaui";
+import { OsButton, OsModal } from "ossaui";
+import { useState } from "react";
 
 const user = () => {
+  const [showBase, setShowBase] = useState(false);
+
   // 退出系统
   const quit = () => {
+    setShowBase(false);
     Taro.clearStorageSync();
     Taro.redirectTo({ url: "/pages/login/index" });
   };
@@ -11,9 +15,20 @@ const user = () => {
   return (
     <>
       <view>我的</view>
-      <OsButton type="primary" size="block" onClick={quit}>
+      <OsButton type="primary" size="block" onClick={() => setShowBase(true)}>
         退出
       </OsButton>
+
+      <OsModal
+        title="确认退出"
+        cancelText="取消"
+        confirmText="确定"
+        content="确认要退出系统吗"
+        isShow={showBase}
+        onCancel={() => setShowBase(false)}
+        onClose={() => setShowBase(false)}
+        onConfirm={quit}
+      ></OsModal>
     </>
   );
 };
