b45dfdbb2418460e2586b95f8541e713cebfccb5
Scathach <scathach0822@hotmail.com>
2023-03-20 20:33:42 +0800

    修复tabbar会遮挡页面的问题

--- src/pages/voteCount/index.tsx
+++ src/pages/voteCount/index.tsx
@@ -1,7 +1,95 @@
+import { View } from "@tarojs/components";
+import Taro from "@tarojs/taro";
+import { OsList, OsSearch, OsTag } from "ossaui";
+import { useEffect, useState } from "react";
+
 const VoteCount = () => {
+  const [voteList, setVoteList] = useState<any[]>([]);
+  const [sortedVoteList, setSortedVoteList] = useState<any[]>([]);
+  const data = Taro.getStorageSync("data");
 
+  // token校验
+  useEffect(() => {
+    const token = Taro.getStorageSync("token") || null;
+    if (token !== null) {
+      Taro.request({
+        url: "http://localhost:8080/login/checkToken",
+        method: "POST",
+        header: { token },
+      }).then((res) => {
+        if (!res) {
+          Taro.redirectTo({
+            url: "/pages/login/index",
+          });
+        }
+      });
+    }
+  });
 
-    return (<>这是投票统计页面</>)
-}
+  // voteList数组监控
+  useEffect(() => {
+    const sortedList = voteList.sort((a, b) => {
+      return b.endTime < a.endTime ? -1 : 1;
+    });
+    setSortedVoteList(sortedList);
+  }, [voteList]);
+
+  useEffect(() => {
+    Taro.request({
+      url: "http://localhost:8080/vote/findAllByCreate",
+      method: "POST",
+      data: { createBy: data.name },
+      success: (res) => {
+        if (res.data.list) {
+          setVoteList(res.data.list);
+        }
+      },
+    });
+  }, []);
+
+  return (
+    <View>
+      <OsSearch
+        placeholder="投票标题搜索"
+        onChange={(e, value) => {
+          // searchByTiele(e, value);
+        }}
+      ></OsSearch>
+      {sortedVoteList.length > 0
+        ? sortedVoteList.map((item, index) => {
+            const nowTime = new Date().valueOf();
+            const endTime = new Date(item.endTime).valueOf();
+            return (
+              <>
+                <OsList
+                  title={item.title}
+                  subTitle={
+                    item.description.length > 18
+                      ? item.description.substring(0, 17) + "..."
+                      : item.description
+                  }
+                  type="custom"
+                  rightIcon="arrows"
+                  onClick={() => {
+                    // goToDetail(index);
+                  }}
+                >
+                  {nowTime >= endTime ? (
+                    <OsTag type="primary" color="error">
+                      已截止
+                    </OsTag>
+                  ) : (
+                    <OsTag type="primary" color="#66CC66">
+                      进行中
+                    </OsTag>
+                  )}
+                </OsList>
+              </>
+            );
+          })
+        : null}
+    </View>
+  );
+};
 
 export default VoteCount;
