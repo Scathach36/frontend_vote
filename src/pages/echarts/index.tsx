9a0255246439472ad3967e0fa4b26beac4fb398c
Scathach <scathach0822@hotmail.com>
2023-03-23 22:43:46 +0800

    投票统计完成

--- src/pages/echarts/index.tsx
+++ src/pages/echarts/index.tsx
@@ -4,7 +4,7 @@ import { View } from "@tarojs/components";
 //@ts-ignore
 import * as echarts from "../../components/ec-canvas/echarts";
 import "./index.scss";
-import { OsList } from "ossaui";
+import { OsList, OsToast } from "ossaui";
 
 const initChart = (canvas, width, height) => {
   const chart = echarts.init(canvas, null, {
@@ -22,6 +22,8 @@ const Echarts = () => {
   const [histogramX, setHistogramX] = useState<string[]>([]);
   const [histogramData, setHistogramData] = useState<number[]>([]);
   const [total, setTotal] = useState<number>(0);
+  const [tickets, setTickets] = useState<any[]>([]);
+  const [voterList, setVoterList] = useState<any[]>([]);
 
   const id = router.params.id;
 
@@ -64,6 +66,14 @@ const Echarts = () => {
         setOptions(res.data.list);
       },
     });
+    Taro.request({
+      url: "http://localhost:8080/vote/findTicketsByVoteId",
+      method: "POST",
+      data: { id: Number(id) },
+      success: (res) => {
+        setTickets(res.data.list);
+      },
+    });
   }, []);
 
   useEffect(() => {
@@ -80,6 +90,34 @@ const Echarts = () => {
     setHistogramData(hisData);
     setHistogramX(hisX);
   }, [options]);
+
+  useEffect(() => {
+    let voters: any[] = [];
+    let votersArr: string[] = [];
+    options.forEach((option, index) => {
+      voters[index] = [];
+      tickets.forEach((ticket) => {
+        if (option.id == ticket.optionId) {
+          voters[index].push(ticket.username);
+        }
+      });
+    });
+
+    voters.forEach((item, index) => {
+      if (item.length == 0) {
+        votersArr[index] = "无";
+      } else {
+        votersArr[index] = "";
+        item.forEach((voter, voterIndex) => {
+          voterIndex == 0
+            ? (votersArr[index] += voter)
+            : (votersArr[index] = votersArr[index] + " " + voter);
+        });
+      }
+    });
+
+    setVoterList(votersArr);
+  }, [tickets, options]);
 
   return (
     <>
@@ -103,10 +141,22 @@ const Echarts = () => {
           {options.map((item, index) => {
             return (
               <>
-                <OsList
-                  title={index + 1 + "." + item.description}
-                  desc={"票数：" + item.number + "票"}
-                ></OsList>
+                {detail && detail.anonymous == 1 && (
+                  <OsList
+                    title={index + 1 + "." + item.description}
+                    desc={"票数：" + item.number + "票"}
+                  ></OsList>
+                )}
+                {detail && detail.anonymous == 0 && (
+                  <>
+                    <OsList
+                      title={index + 1 + "." + item.description}
+                      desc={"票数：" + item.number + "票"}
+                      rightIcon="arrows"
+                      subTitle={"参与者：" + voterList[index]}
+                    ></OsList>
+                  </>
+                )}
               </>
             );
           })}
